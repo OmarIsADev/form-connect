660de9b00bde399f7f01be1cf0a1e30aecc7edb7
gamerlord295 <saadomar343@gmail.com>
2024-04-14 21:31:08 +0200

    added 2 sections

---
+++ src/components/frameworks.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import vue from "../assets/vue.png";
+import html from "../assets/html.png";
+import angular from "../assets/angular.png";
+import react from "../assets/react.png";
+import nuxt from "../assets/nuxt.png";
+import next from "../assets/nextjs.png";
+
+const data = [
+    {
+      name: "html",
+      img: html,
+    },
+    {
+        name: "react",
+        img: react,
+    },
+    {
+        name: "vue",
+        img: vue,
+    },
+    {
+        name: "angular",
+        img: angular,
+    },
+    {
+      name: "nuxt",
+      img: nuxt,
+    },
+];
+
+export default function Frameworks() {
+  return (
+    <div className="tech">
+      <h2>Works with every technology</h2>
+      <div>
+        {data.map((item) => (
+          <img src={item.img} alt={item.name} />
+        ))}
+        <img src={next} alt="nextjs" className="nextjs" />
+      </div>
+    </div>
+  );
+}
---
+++ src/components/header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div>
+        <p>Logo</p>
+        <h1>Form Connect</h1>
+      </div>
+      <div>
+        <a>Example</a>
+        <a>Features</a>
+        <a>About</a>
+      </div>
+    </div>
+  );
+}
